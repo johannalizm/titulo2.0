112a3033663c7706f70f8570c59a9ececc900231
johannalizm <jovi_list@hotmail.com>
2018-11-06 17:22:15 -0300

    manual uso

--- resies/config/initializers/assets.rb
+++ resies/config/initializers/assets.rb
@@ -14,6 +14,7 @@ Rails.application.config.assets.precompile += %w( preg.css)
 Rails.application.config.assets.precompile += %w( jr.js)
 Rails.application.config.assets.precompile += %w( pre.js)
 Rails.application.config.assets.precompile += %w( menu.css)
+Rails.application.config.assets.precompile += %w( manualresies.css)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
--- resies/config/routes.rb
+++ resies/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   resources :pregunta
   resources :instituciones
   devise_for :users, controllers: {:registrations => 'user/registrations', :confirmations =>'user/confirmations'}
   root to: 'welcome#index'
+
+  get 'manual/', to: 'manual#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
